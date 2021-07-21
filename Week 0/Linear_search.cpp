acee26bd0c102d754d2bfae30d9b3ce021ec9a88
Kaustav <kaustavroy979@gmail.com>
2021-07-21 18:36:25 +0530

    Linear search

--- Week 0/Linear_search.cpp
+++ Week 0/Linear_search.cpp
@@ -1,1 +1,26 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void main()
+{
+    int n;
+    cin>>n;
+    int arr[n],i=0,val;
+    int n1 = n;
+    while (n--)
+    {
+        cin>>arr[i++];
+    }
+    cin>>val;
+    for (int i = 0; i < n1; i++)
+    {
+        if(arr[i]==val)
+        {
+            cout<<"Found at index "<<i<<endl;
+        }
+    }
+    if(i == val)
+    {
+        cout<<"Not found"<<endl;
+    }
+    
+}
